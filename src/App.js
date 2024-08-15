81483a280697b31ba647f45d0865f7a50fe7d8a6
ArwaYearub <Arwa.Yaareb@gmail.com>
2024-08-15 17:28:55 +0300

    importing into app.js
    importing todoinput and todolist into app.js and importing todoCard into Todolist, deleting app.css for good, adding the style to index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
   );
 }
 
